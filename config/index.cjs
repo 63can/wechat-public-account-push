87b6c4d3bdd29169a31685bbb0257fb2c86d0fa5
volcano <liucan1113@163.com>
2022-09-26 17:21:08 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -69,9 +69,9 @@ const USER_CONFIG = {
       // 你想对他发送的模板消息的模板ID
       useTemplateId: 'hGH9Hd5Z34L9ZEsdxuG-KWPM1wdYh0m1fYZzy4HsEHE',
       // 所在省份
-      province: '北京',
+      province: '吉林',
       // 所在城市
-      city: '北京',
+      city: '通化',
       // 新历生日, 仅用作获取星座运势, 格式必须
       horoscopeDate: '11-13',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
@@ -82,14 +82,14 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '测试', year: '1988', date: '10-05',
+          type: '*生日', name: '测试刘-阴历', year: '1988', date: '10-05',
         },
         {
-          type: '节日', name: '纪念日', year: '2021', date: '11-27',
+          type: '节日', name: '一周年纪念日', year: '2021', date: '11-27',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '测试', year: '1988', date: '11-13',
+          type: '生日', name: '测试刘-阳历', year: '1988', date: '11-13',
         },
         {
           type: '节日', name: '被搭讪纪念日', year: '2021', date: '09-01',
@@ -98,11 +98,7 @@ const USER_CONFIG = {
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
-        // 退伍日
-        { keyword: 'ex_day', date: '2022-09-10' }
+        { keyword: 'love_day', date: '2021-11-27' },
       ],
       // 课程表相关配置
       // 如果courseSchedule不存在或者为空（null）则认为没有课程
