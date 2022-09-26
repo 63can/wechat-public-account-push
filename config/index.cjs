16f53e3c9109969d8a4f62ed3abc870fd21e31a2
volcano <liucan1113@163.com>
2022-09-26 17:47:58 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -73,23 +73,22 @@ const USER_CONFIG = {
       // 所在城市
       city: '通化',
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '11-13',
+      horoscopeDate: '10-12',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: 'http://astro.sina.com.cn/fate_day_Libra/',
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '*生日', name: '小曲农历生日', year: '1998', date: '08-20',
+          type: '生日', name: '小曲宝生日', year: '1998', date: '10-12',
+        },
+         {
+          type: '生日', name: '小火山生日', year: '1988', date: '11-13',
         },
         {
           type: '节日', name: '一周年纪念日', year: '2021', date: '11-27',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '小曲阳历生日', year: '1998', date: '10-12',
         },
         {
           type: '节日', name: '新年', year: '2023', date: '01-01',
@@ -175,7 +174,23 @@ const USER_CONFIG = {
       horoscopeDate: '10-12',
       horoscopeDateType: '今日',
       openUrl: 'http://astro.sina.com.cn/fate_day_Libra/',
-      festivals: [],
+      festivals: [ {
+          type: '生日', name: '小曲宝生日', year: '1998', date: '10-12',
+        },
+         {
+          type: '生日', name: '小火山生日', year: '1988', date: '11-13',
+        },
+        {
+          type: '节日', name: '一周年纪念日', year: '2021', date: '11-27',
+        },
+        {
+          type: '节日', name: '新年', year: '2023', date: '01-01',
+        }
+      ],
+      // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2021-11-27' },],
       customizedDateList: [],
       courseSchedule: null
     },
